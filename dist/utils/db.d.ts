68c69e236e978c194c3da62dad8e10ad7f6563a6
mdalmamunit427 <mdalmamunit427@gmail.com>
2025-09-17 15:06:58 +0600

    db update

--- dist/utils/db.d.ts
+++ dist/utils/db.d.ts
@@ -1,3 +1,4 @@
-declare const dbConnect: () => Promise<void>;
+import mongoose from "mongoose";
+declare const dbConnect: () => Promise<typeof mongoose>;
 export default dbConnect;
 //# sourceMappingURL=db.d.ts.map
